99e1fb937e22d7d83a4afd9331d101b4c85d4982
Peter <Peter@gmail.com>
2018-11-12 21:31:46 +0800

    fix Average4

--- src/Compute/Average4.java
+++ src/Compute/Average4.java
@@ -1,10 +1,10 @@
 package Compute;
-
+import java.util.Scanner;
 public class Average4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
 	}
 
 }
